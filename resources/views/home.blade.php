57ad9e8a61811ba7fc8682098437fc38317a9940
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-11 17:06:10 +0100

    Exercise completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,28 @@
 </head>
 
 <body>
-
+    <!-- Section Movies -->
+    <section id="movies">
+    <!-- Row -->
+    <div class="row">
+        <!-- Foreach -->
+        @foreach ($movies as $movie)
+            <!-- Col -->
+            <div class="col-3">
+                <!-- Card -->
+                <div class="card">
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->original_title}}</h6>
+                        <p class="card-text">{{$movie->nationality}}</p>
+                        <p class="card-text">{{$movie->date}}</p>
+                        <p class="card-text">{{$movie->vote}}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</section>
 </body>
 </html>
