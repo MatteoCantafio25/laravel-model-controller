59999751c34f57deca26af61ef6e469be17c42cb
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-11 16:31:03 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Matteo Cantafio">
+    <meta name="description" content="Larvel Model & Controller">
+    <title>Larvel Model & Controller</title>
+
+    <!-- App Js -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+</body>
+</html>
